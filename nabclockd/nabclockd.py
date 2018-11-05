beb7d3c1829e6d0bfbe8bc7260030ace008bd76c
Paul Guyot <pguyot@kallisys.net>
2018-11-05 23:32:28 +0100

    Fixes in clock logic with new tests

--- nabclockd/nabclockd.py
+++ nabclockd/nabclockd.py
@@ -2,30 +2,60 @@ import sys, signal, asyncio, json, datetime
 from nabcommon import nabservice
 
 class NabClockd(nabservice.NabService,asyncio.Protocol):
-  NABD_PORT = 10543
-
   def __init__(self):
     super().__init__()
     from . import models
     self.config = models.Config.load()
     self.loop_cv = asyncio.Condition()
+    self.loop = None
     self.asleep = None
     self.last_chime = None
     signal.signal(signal.SIGUSR1, self.signal_handler)
 
-  def signal_handler(sig, frame):
+  def signal_handler(self, sig, frame):
+    self.loop.call_soon_threadsafe(self.reload_config())
+
+  async def reload_config(self):
     from django.core.cache import cache
     cache.clear()
+    from . import models
     self.config = models.Config.load()
+    async with self.loop_cv:
+      self.loop_cv.notify()
   
   def valid_time(self, now):
-    now > datetime.datetime(2018,11,1)
+    return now > datetime.datetime(2018,11,1)
   
   def chime(self, hour):
     now = datetime.datetime.now()
     expiration = now + datetime.timedelta(minutes=3)
     # TODO : audio sequence for hour.
-    self.writer.write('{"type":"command","sequence":[{"audio":[],"choregraphy":"streaming"}],"expiration":' + expiration.isoformat() + '\r\n')
+    packet = '{"type":"command","sequence":[{"audio":[],"choregraphy":"streaming"}],"expiration":"' + expiration.isoformat() + '"}\r\n'
+    self.writer.write(packet.encode('utf8'))
+
+  def clock_response(self, now):
+    response = []
+    if self.valid_time(now):
+      should_sleep = None
+      if self.config.wakeup_hour != None and \
+         self.config.sleep_hour != None and \
+         self.config.wakeup_min != None and \
+         self.config.sleep_min != None:
+        if (self.config.wakeup_hour, self.config.wakeup_min) < (self.config.sleep_hour, self.config.sleep_min):
+          should_sleep = (now.hour, now.minute) < (self.config.wakeup_hour, self.config.wakeup_min) or (now.hour, now.minute) >= (self.config.sleep_hour, self.config.sleep_min)
+        else:
+          should_sleep = (now.hour, now.minute) < (self.config.wakeup_hour, self.config.wakeup_min) and (now.hour, now.minute) >= (self.config.sleep_hour, self.config.sleep_min)
+      if should_sleep != None and self.asleep != None and should_sleep != self.asleep:
+        if should_sleep:
+          response.append('sleep')
+        else:
+          response.append('wakeup')
+      if (should_sleep == None or should_sleep == False) and now.minute == 0 and self.config.chime_hour:
+        if self.last_chime != now.hour:
+          response.append('chime')
+      if now.minute > 5: # account for time drifts
+        response.append('reset_last_chime')
+    return response
 
   async def clock_loop(self):
     try:
@@ -33,23 +63,18 @@ class NabClockd(nabservice.NabService,asyncio.Protocol):
         while self.running:
           try:
             now = datetime.datetime.now()
-            if self.valid_time(now):
-              should_sleep = None
-              if self.config.wakeup_hour and self.config.sleep_hour and self.config.wakeup_min and self.config.sleep_min:
-                if (self.config.wakeup_hour, self.config.wakeup_min) < (self.config.sleep_hour, self.config.sleep_min):
-                  should_sleep = (now.hour, now_min) <= (self.config.wakeup_hour, self.config.wakeup_min) or (now.hour, now_min) >= (self.config.sleep_hour, self.config.sleep_min)
-                else:
-                  should_sleep = (now.hour, now_min) <= (self.config.wakeup_hour, self.config.wakeup_min) and (now.hour, now_min) >= (self.config.sleep_hour, self.config.sleep_min)
-              if should_sleep != None and asleep != None and should_sleep != asleep:
-                if should_sleep:
-                  self.writer.write('{"type":"sleep"}\r\n')
-                else:
-                  self.writer.write('{"type":"wakeup"}\r\n')
-              if should_sleep == None or should_sleep == False and now.min == 0 and self.config.chime_hour:
-                if self.last_chime != now.hour:
-                  chime(now.hour)
-                  self.last_chime = now.hour
-              if now.min > 5: # account for time drifts
+            response = self.clock_response(now)
+            for r in response:
+              if r == 'sleep':
+                self.writer.write(b'{"type":"sleep"}\r\n')
+                self.asleep = None
+              elif r == 'wakeup':
+                self.writer.write(b'{"type":"wakeup"}\r\n')
+                self.asleep = None
+              elif r == 'chime':
+                self.chime(now.hour)
+                self.last_chime = now.hour
+              elif r == 'reset_last_chime':
                 self.last_chime = None
             sleep_amount = 60 - now.second
             await asyncio.wait_for(self.loop_cv.wait(), sleep_amount)
@@ -72,10 +97,10 @@ class NabClockd(nabservice.NabService,asyncio.Protocol):
 
   def run(self):
     super().connect()
-    loop = asyncio.get_event_loop()
-    clock_task = loop.create_task(self.clock_loop())
+    self.loop = asyncio.get_event_loop()
+    clock_task = self.loop.create_task(self.clock_loop())
     try:
-      loop.run_forever()
+      self.loop.run_forever()
       if clock_task.done():
         ex = clock_task.exception()
         if ex:
@@ -84,11 +109,11 @@ class NabClockd(nabservice.NabService,asyncio.Protocol):
       pass
     finally:
       self.writer.close()
-      loop.run_until_complete(self.stop_clock_loop())
-      tasks = asyncio.all_tasks(loop)
+      self.loop.run_until_complete(self.stop_clock_loop())
+      tasks = asyncio.all_tasks(self.loop)
       for t in [t for t in tasks if not (t.done() or t.cancelled())]:
-        loop.run_until_complete(t)    # give canceled tasks the last chance to run
-      loop.close()
+        self.loop.run_until_complete(t)    # give canceled tasks the last chance to run
+      self.loop.close()
 
 if __name__ == '__main__':
   service = NabClockd()
