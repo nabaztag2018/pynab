684c6adab92c96d0a2543a75902f78ecf798fd70
Paul Guyot <pguyot@kallisys.net>
2018-11-17 14:48:32 +0100

    Fix ALSA sound

--- nabd/sound_alsa.py
+++ nabd/sound_alsa.py
@@ -6,13 +6,49 @@ from concurrent.futures import ThreadPoolExecutor
 from .sound import Sound
 
 class SoundAlsa(Sound):
-  ALSA_DEVICE = 'default'
-
   def __init__(self):
-    self.device = alsaaudio.PCM(device=SoundAlsa.ALSA_DEVICE)
+    self.device = SoundAlsa.select_device()
     self.executor = ThreadPoolExecutor(max_workers=1)
     self.play_future = None
     self.currently_playing = False
+
+  @staticmethod
+  def select_device():
+    """
+    Automatically select a suitable ALSA device by trying to configure them.
+    """
+    for device in alsaaudio.pcms():
+      if device != 'null':
+        if SoundAlsa.test_device(device):
+          return device
+    print('No suitable ALSA device!')
+    return 'null'
+
+  @staticmethod
+  def test_device(device):
+    """
+    Test an ALSA device, making sure it handles both stereo and mono and
+    both 44.1KHz and 22.05KHz. On a typical RPI configuration, default with
+    hifiberry card is not configured to do software-mono, so we'll use
+    'sysdefault:CARD=sndrpihifiberry' instead.
+    """
+    try:
+      dev = alsaaudio.PCM(device=device)
+      if dev.setchannels(2) != 2:
+        return False
+      if dev.setchannels(1) != 1:
+        return False
+      if dev.setrate(44100) != 44100:
+        return False
+      if dev.setrate(22050) != 22050:
+        return False
+      if dev.setformat(alsaaudio.PCM_FORMAT_S16_LE) != alsaaudio.PCM_FORMAT_S16_LE:
+        return False
+    except alsaaudio.ALSAAudioError:
+      return False
+    finally:
+      dev.close()
+    return True
 
   async def start_preloaded(self, filename):
     await self.stop()
@@ -21,44 +57,59 @@ class SoundAlsa(Sound):
 
   def _do_start(self, filename):
     try:
+      device = alsaaudio.PCM(device=self.device)
       if filename.endswith('.wav'):
-        with wave.open(args[0], 'rb') as f:
-          self._setup_device(f.getnchannels(), f.getframerate(), f.getsampwidth())
-          periodsize = int(f.getframerate() / 8)
+        with wave.open(filename, 'rb') as f:
+          channels = f.getnchannels()
+          width = f.getsampwidth()
+          rate = f.getframerate()
+          self._setup_device(device, channels, rate, width)
+          periodsize = int(rate / 10) # 1/10th of second
+          device.setperiodsize(periodsize)
           data = f.readframes(periodsize)
+          chunksize = periodsize * channels * width
           while data and self.currently_playing:
-            self.device.write(data)
+            if len(data) < chunksize:
+              data = data + bytearray(chunksize - len(data))
+            device.write(data)
             data = f.readframes(periodsize)
       elif filename.endswith('.mp3'):
         mp3 = Mpg123(filename)
         rate, channels, encoding = mp3.get_format()
         width = mp3.get_width_by_encoding(encoding)
-        self._setup_device(channels, rate, width)
-        for frame in mp3.iter_frames():
-          self.device.write(frame)
+        self._setup_device(device, channels, rate, width)
+        chunksize = None
+        for chunk in mp3.iter_frames():
+          if chunksize == None:
+            chunksize = len(chunk)
+            periodsize = int(chunksize / width / channels)
+            device.setperiodsize(periodsize)
+          if len(chunk) < chunksize:
+            chunk = chunk + bytearray(chunksize - len(chunk))
+          device.write(chunk)
+          if not self.currently_playing:
+            break
     finally:
       self.currently_playing = False
+      device.close()
 
-  def _setup_device(self, channels, rate, width):
+  def _setup_device(self, device, channels, rate, width):
     # Set attributes
-    self.device.setchannels(channels)
-    self.device.setrate(rate)
+    device.setchannels(channels)
+    device.setrate(rate)
 
     # 8bit is unsigned in wav files
     if width == 1:
-        self.device.setformat(alsaaudio.PCM_FORMAT_U8)
+        device.setformat(alsaaudio.PCM_FORMAT_U8)
     # Otherwise we assume signed data, little endian
     elif width == 2:
-        self.device.setformat(alsaaudio.PCM_FORMAT_S16_LE)
+        device.setformat(alsaaudio.PCM_FORMAT_S16_LE)
     elif width == 3:
-        self.device.setformat(alsaaudio.PCM_FORMAT_S24_3LE)
+        device.setformat(alsaaudio.PCM_FORMAT_S24_3LE)
     elif width == 4:
-        self.device.setformat(alsaaudio.PCM_FORMAT_S32_LE)
+        device.setformat(alsaaudio.PCM_FORMAT_S32_LE)
     else:
         raise ValueError('Unsupported format')
-
-    periodsize = int(rate / 8)
-    self.device.setperiodsize(periodsize)
 
   async def stop(self):
     if self.currently_playing:
---
+++ nabd/tests/sound_alsa_test.py
@@ -0,0 +1,44 @@
+import unittest
+import asyncio
+import sys
+import pytest
+
+@pytest.mark.skipif(sys.platform != 'linux', reason="Alsa is only available on Linux")
+class TestSound(unittest.TestCase):
+  def setUp(self):
+    from nabd.sound_alsa import SoundAlsa
+    self.loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(self.loop)
+    self.sound = SoundAlsa()
+
+  def test_mp3(self):
+    start_task = self.loop.create_task(self.sound.start('choreographies/3notesE5A5C6.mp3'))
+    self.loop.run_until_complete(start_task)
+    wait_task = self.loop.create_task(self.sound.wait_until_done())
+    self.loop.run_until_complete(wait_task)
+
+  def test_two_mp3s(self):
+    start_task = self.loop.create_task(self.sound.start('choreographies/3notesA4G5G5.mp3'))
+    self.loop.run_until_complete(start_task)
+    wait_task = self.loop.create_task(self.sound.wait_until_done())
+    self.loop.run_until_complete(wait_task)
+    start_task = self.loop.create_task(self.sound.start('choreographies/2notesE5E4.mp3'))
+    self.loop.run_until_complete(start_task)
+    wait_task = self.loop.create_task(self.sound.wait_until_done())
+    self.loop.run_until_complete(wait_task)
+
+  def test_two_wavs(self):
+    start_task = self.loop.create_task(self.sound.start('nabmastodond/communion.wav'))
+    self.loop.run_until_complete(start_task)
+    wait_task = self.loop.create_task(self.sound.wait_until_done())
+    self.loop.run_until_complete(wait_task)
+    start_task = self.loop.create_task(self.sound.start('nabmastodond/communion.wav'))
+    self.loop.run_until_complete(start_task)
+    wait_task = self.loop.create_task(self.sound.wait_until_done())
+    self.loop.run_until_complete(wait_task)
+
+  def test_wav(self):
+    start_task = self.loop.create_task(self.sound.start('nabmastodond/communion.wav'))
+    self.loop.run_until_complete(start_task)
+    wait_task = self.loop.create_task(self.sound.wait_until_done())
+    self.loop.run_until_complete(wait_task)
