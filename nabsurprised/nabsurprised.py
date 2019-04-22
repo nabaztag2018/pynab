82b1487a1a19f349192ca0c8461990d68c39ea13
Paul Guyot <pguyot@kallisys.net>
2019-04-22 20:00:26 +0200

    Cleanup

--- nabsurprised/nabsurprised.py
+++ nabsurprised/nabsurprised.py
@@ -1,24 +1,19 @@
-import sys, asyncio, datetime, random
+import sys, asyncio, random
 from nabcommon.nabservice import NabRandomService
 
 class NabSurprised(NabRandomService):
-  DAEMON_PIDFILE = '/var/run/nabsurprised.pid'
-
   def get_config(self):
     from . import models
     config = models.Config.load()
-    print('loading surprise config, next = {next}'.format(next = config.next_surprise))
     return (config.next_surprise, config.surprise_frequency)
 
   def update_next(self, next_date, next_args):
     from . import models
     config = models.Config.load()
-    print('updating surprise config, next = {next}'.format(next = next))
     config.next_surprise = next_date
     config.save()
 
   def perform(self, expiration, args):
-    print('performing surprise !')
     packet = '{"type":"message","signature":{"audio":["nabsurprised/respirations/*.mp3"]},"body":[{"audio":["nabsurprised/*.mp3"]}],"expiration":"' + expiration.isoformat() + '"}\r\n'
     self.writer.write(packet.encode('utf8'))
 
