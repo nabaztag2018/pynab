85e1b56eca4aa82d2a46f85b9e1a18be6af89d26
Paul Guyot <pguyot@kallisys.net>
2019-04-27 22:18:01 +0200

    Fix skipped test on Travis

--- nabd/tests/leds_neopixel_test.py
+++ nabd/tests/leds_neopixel_test.py
@@ -1,10 +1,10 @@
 import unittest, time, pytest, sys, platform
 from nabd.leds import Leds
-from nabd.leds_neopixel import LedsNeoPixel
 
 @pytest.mark.skipif(sys.platform != 'linux' or not 'arm' in platform.machine(), reason="Neopixel test only makes sens on a physical Nabaztag")
 class TestLedsNeopixel(unittest.TestCase):
   def test_set_one(self):
+    from nabd.leds_neopixel import LedsNeoPixel
     leds = LedsNeoPixel()
     for (r, g, b) in ((255,255,255), (255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0), (255, 0, 255), (0, 255, 255), (0, 0, 0)):
       for i in range(0, 5):
@@ -13,6 +13,7 @@ class TestLedsNeopixel(unittest.TestCase):
         time.sleep(1)
 
   def test_set_all(self):
+    from nabd.leds_neopixel import LedsNeoPixel
     leds = LedsNeoPixel()
     for (r, g, b) in ((255,255,255), (255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0), (255, 0, 255), (0, 255, 255), (0, 0, 0)):
       for i in range(0, 5):
