3018d1df0b53a97e7909ac798ba860f4620fc53f
Paul Guyot <pguyot@kallisys.net>
2019-04-27 21:59:26 +0200

    Run ldconfig after installing kaldi

--- install_kaldi.sh
+++ install_kaldi.sh
@@ -10,10 +10,12 @@ case `uname` in
     case `uname -m` in
       "x86_64")
         wget -q -O - "https://github.com/pguyot/kaldi/releases/download/v5.4.1/kaldi-c3260f2-linux_x86_64-vfp.tar.xz" | sudo tar xJ -C /
+        sudo ldconfig
         ;;
       "armv6l")
         # This probably is a Pi Zero
         wget -q -O - "https://github.com/pguyot/kaldi/releases/download/v5.4.1/kaldi-c3260f2-linux_armv6l-vfp.tar.xz" | sudo tar xJ -C /
+        sudo ldconfig
         ;;
       *)
         echo "Unknown architecture. Please install kaldi and make it available to pkg-config"
