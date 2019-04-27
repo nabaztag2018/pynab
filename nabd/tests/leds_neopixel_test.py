09546a202b09b74816cfe53a5ac089fcf2fabc67
Paul Guyot <pguyot@kallisys.net>
2019-04-27 21:47:29 +0200

    New hardware test for leds

---
+++ nabd/tests/leds_neopixel_test.py
@@ -0,0 +1,21 @@
+import unittest, time, pytest, sys, platform
+from nabd.leds import Leds
+from nabd.leds_neopixel import LedsNeoPixel
+
+@pytest.mark.skipif(sys.platform != 'linux' or not 'arm' in platform.machine(), reason="Neopixel test only makes sens on a physical Nabaztag")
+class TestLedsNeopixel(unittest.TestCase):
+  def test_set_one(self):
+    leds = LedsNeoPixel()
+    for (r, g, b) in ((255,255,255), (255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0), (255, 0, 255), (0, 255, 255), (0, 0, 0)):
+      for i in range(0, 5):
+        leds.do_set(i, r, g, b)
+        leds.do_show()
+        time.sleep(1)
+
+  def test_set_all(self):
+    leds = LedsNeoPixel()
+    for (r, g, b) in ((255,255,255), (255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0), (255, 0, 255), (0, 255, 255), (0, 0, 0)):
+      for i in range(0, 5):
+        leds.do_set(i, r, g, b)
+      leds.do_show()
+      time.sleep(1)
