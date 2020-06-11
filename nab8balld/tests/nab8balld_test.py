38d1f1a48f335a039ff0b163a0e966d05685fc9a
Paul Guyot <pguyot@kallisys.net>
2020-06-11 12:02:59 +0200

    Additional test

--- nab8balld/tests/nab8balld_test.py
+++ nab8balld/tests/nab8balld_test.py
@@ -1,7 +1,28 @@
+import unittest
+import json
 import pytest
+from asgiref.sync import async_to_sync
 from nab8balld.nab8balld import Nab8Balld
 from nabd.tests.utils import close_old_async_connections
-from nabd.tests.mock import NabdMockTestCase
+from nabd.tests.mock import MockWriter, NabdMockTestCase
+
+
+@pytest.mark.django_db
+class TestNab8balld(unittest.TestCase):
+    def tearDown(self):
+        close_old_async_connections()
+
+    def test_perform(self):
+        service = Nab8Balld()
+        writer = MockWriter()
+        service.writer = writer
+        async_to_sync(service.perform)()
+        self.assertEqual(len(writer.written), 1)
+        packet = writer.written[0]
+        packet_json = json.loads(packet.decode("utf8"))
+        self.assertEqual(packet_json["type"], "message")
+        self.assertFalse("signature" in packet_json)
+        self.assertTrue("body" in packet_json)
 
 
 @pytest.mark.django_db
