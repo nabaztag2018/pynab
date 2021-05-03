nabaztag2018/pynab

commit bfefe9cedee1bb99370b9f53d708844f89895a99

Limit localization to original 8 locales

install.sh
@@ -276,14 +276,16 @@ if [ $upgrade -eq 1 ]; then
 fi
 venv/bin/python manage.py migrate
 
+all_locales="--locale=fr_FR --locale=de_DE --locale=en_US --locale=en_GB --locale=it_IT --locale=es_ES --locale=ja_jp --locale=pt_BR"
+
 if [ $upgrade -eq 0 ]; then
-  venv/bin/django-admin compilemessages
+  venv/bin/django-admin compilemessages ${all_locales}
 else
   echo "Updating localization messages - 11/14" > /tmp/pynab.upgrade
   for module in nab*/locale; do
     (
       cd `dirname ${module}`
-      ../venv/bin/django-admin compilemessages
+      ../venv/bin/django-admin compilemessages ${all_locales}
     )
   done
 fi

commit 76bb9139771a2baf56559de3781b5f866a76e078

Fix invocation of compilemessages
Broken in bfefe9cedee1bb99370b9f53d708844f89895a99

install.sh
@@ -279,7 +279,7 @@ if [ $upgrade -eq 1 ]; then
 fi
 venv/bin/python manage.py migrate
 
-all_locales="--locale=fr_FR --locale=de_DE --locale=en_US --locale=en_GB --locale=it_IT --locale=es_ES --locale=ja_jp --locale=pt_BR"
+all_locales="--locale=fr_FR --locale=de_DE --locale=en_US --locale=en_GB --locale=it_IT --locale=es_ES --locale=ja_jp --locale=pt_BR --locale=de --locale=en --locale=es --locale=fr --locale=it --locale=ja --locale=pt"
 
 if [ $upgrade -eq 0 ]; then
   venv/bin/django-admin compilemessages ${all_locales}