245203e17fa7411d0511f53069a53cae1084d244
Nicolas Guillaumin <nicolas+git@guillaumin.me>
2021-05-08 12:19:16 +0200

    Remove now unused import

--- nabweb/urls.py
+++ nabweb/urls.py
@@ -13,7 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-import os
 
 from django.apps import apps
 from django.urls import include, path
