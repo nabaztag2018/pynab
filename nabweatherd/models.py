663aa3a23b4b9682e2cf712d9d9ec6c8235c28c7
Olivier <olivier@readiymate.com>
2021-05-09 15:28:28 +0200

    Fix default value for Air and Weather forecast

--- nabweatherd/models.py
+++ nabweatherd/models.py
@@ -5,13 +5,13 @@ from nabcommon import singleton_model
 
 
 class Config(singleton_model.SingletonModel):
-    location = models.TextField(null=True)
-    location_user_friendly = models.TextField(null=True)
+    location = models.TextField(null=True, default='{"insee": "29039", "name": "Concarneau", "lat": 47.87528, "lon": -3.91889, "country": "FR", "admin": "Bretagne", "admin2": "29", "postCode": "29900"}')
+    location_user_friendly = models.TextField(null='Concarneau - Bretagne (29) - FR')
     unit = models.IntegerField(null=False, default=1)
     next_performance_date = models.DateTimeField(null=True)
     next_performance_type = models.TextField(null=True)
-    weather_animation_type = models.TextField(null=True, default="nothing")
-    weather_frequency = models.IntegerField(default=0)
+    weather_animation_type = models.TextField(null=True, default="weather_and_rain")
+    weather_frequency = models.IntegerField(default=3)
 
     next_performance_weather_vocal_date = models.DateTimeField(null=True)
     next_performance_weather_vocal_flag = models.IntegerField(
