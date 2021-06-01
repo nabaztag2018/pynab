ff680bd5ed94b29a9b74906cb0dc1068063a2964
Nabaztag <44699852+nabaztag2018@users.noreply.github.com>
2021-06-01 10:23:39 +0200

    Revert "Revamp system information and upgrade pages"

--- nabweb/views.py
+++ nabweb/views.py
@@ -4,7 +4,6 @@ import base64
 import datetime
 import json
 import os
-import platform
 import re
 import subprocess
 
@@ -325,28 +324,19 @@ class NabWebSytemInfoView(BaseView):
         return "nabweb/system-info/index.html"
 
     def get_os_info(self):
-        version = "(unknown)"
+        version = "unknown"
         try:
-            with open("/etc/os-release") as release_f:
-                line = release_f.readline()
+            with open("/etc/os-release") as release:
+                line = release.readline()
                 matchObj = re.match(r'PRETTY_NAME="(.+)"$', line, re.M)
                 if matchObj:
                     version = matchObj.group(1)
         except FileNotFoundError:
             pass
-        kernel_release = platform.release()
-        kernel_build = platform.version()
-        kernel_machine = platform.machine()
-        matchObj = re.match(r"#[0-9]+", kernel_build)
-        if matchObj:
-            kernel_build = matchObj.group()
-        version = (
-            f"{version} - "
-            f"Kernel {kernel_release} {kernel_build} {kernel_machine}"
-        )
+        kernel_release = os.popen("uname -rm").read().rstrip()
+        version = version + " - Kernel " + kernel_release
         hostname = os.popen("hostname -a").read().rstrip()
         ip_address = os.popen("hostname -I").read().rstrip()
-        wifi_essid = os.popen("iwgetid -r").read().rstrip()
         try:
             with open("/proc/uptime", "r") as uptime_f:
                 uptime = int(float(uptime_f.readline().split()[0]))
@@ -359,7 +349,6 @@ class NabWebSytemInfoView(BaseView):
             "version": version,
             "hostname": hostname,
             "address": ip_address,
-            "network": wifi_essid,
             "uptime": uptime,
             "ssh": ssh_state,
         }
@@ -369,7 +358,7 @@ class NabWebSytemInfoView(BaseView):
             with open("/proc/device-tree/model") as model_f:
                 model = model_f.readline()
         except (FileNotFoundError):
-            model = "(unknown)"
+            model = "unknown"
         return {"model": model}
 
     def get_context(self):
@@ -433,7 +422,7 @@ class GitInfo:
         "sound_driver": "Tagtagtag sound card driver",
         "ears_driver": "Ears driver",
         "rfid_driver": "RFID reader driver",
-        "nabblockly": "NabBlockly",
+        "nabblockly": "Nabblockly",
     }
 
     @staticmethod
