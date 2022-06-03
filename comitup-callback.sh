4b8e3d925fc233409c0e697f1b0f098c29b5c895
Olivier Mevel <mevelolivier@gmail.com>
2022-06-03 14:38:58 +0200

    Playing a little taichi when Nabaztag is connected to the Wifi network

--- comitup-callback.sh
+++ comitup-callback.sh
@@ -3,4 +3,5 @@
 if [ "$1" == "CONNECTED" ]; then
 	sudo systemctl restart nabairqualityd
 	sudo systemctl restart nabweatherd
+	echo '{"type":"command","sequence":[{"choreography":"nabtaichid/taichi.chor"}]}' | nc -4 -w 5 -v localhost 10543
 fi
