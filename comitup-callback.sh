624b13e480cf8243982af5098e4ae43dd4b6cbb1
Olivier Mevel <mevelolivier@gmail.com>
2022-06-04 16:22:44 +0200

    Added nabifftd to labels.yml + added a blue pulse when the rabbit is in hotspot mode

--- comitup-callback.sh
+++ comitup-callback.sh
@@ -5,3 +5,7 @@ if [ "$1" == "CONNECTED" ]; then
 	sudo systemctl restart nabweatherd
 	echo '{"type":"command","sequence":[{"choreography":"nabtaichid/taichi.chor"}]}' | nc -4 -w 5 -v localhost 10543
 fi
+
+if [ "$1" == "HOTSPOT" ]; then
+	echo '{"type":"command","sequence":[{"choreography":"nabd/rfid.chor"}]}' | nc -4 -w 5 -v localhost 10543
+fi
