36d887ce3676a615004b91534eb16b837d3120d4
Paul Guyot <pguyot@kallisys.net>
2022-06-05 00:11:21 +0200

    Start nabd first with start_all (#354)

--- nabd/management/commands/_start_stop_all.py
+++ nabd/management/commands/_start_stop_all.py
@@ -5,27 +5,26 @@ from django.core.management.base import BaseCommand, CommandError
 
 
 class StartStopCommand(BaseCommand):
+    def __process_one(self, app, command):
+        service_file = os.path.join(settings.BASE_DIR, app, app + ".service")
+        if os.path.exists(service_file):
+            self.stdout.write(f"{app}", ending="")
+            r = os.system(f"service {app} {command}")
+            if r != 0:
+                self.stdout.write(self.style.ERROR(" FAILED"))
+                return 1
+            else:
+                self.stdout.write(self.style.SUCCESS(" OK"))
+        return 0
+
     def do_handle(self, command):
         failed_count = 0
+        if command == "start":
+            failed_count += self.__process_one("nabd", command)
         for app in settings.INSTALLED_APPS:
             if app != "nabd":
-                service_file = os.path.join(
-                    settings.BASE_DIR, app, app + ".service"
-                )
-                if os.path.exists(service_file):
-                    self.stdout.write(f"{app}", ending="")
-                    r = os.system(f"service {app} {command}")
-                    if r != 0:
-                        self.stdout.write(self.style.ERROR(" FAILED"))
-                        failed_count += 1
-                    else:
-                        self.stdout.write(self.style.SUCCESS(" OK"))
-        self.stdout.write("nabd", ending="")
-        r = os.system(f"service nabd {command}")
-        if r != 0:
-            self.stdout.write(self.style.ERROR(" FAILED"))
-            failed_count += 1
-        else:
-            self.stdout.write(self.style.SUCCESS(" OK"))
+                failed_count += self.__process_one(app, command)
+        if command != "start":
+            failed_count += self.__process_one("nabd", command)
         if failed_count > 0:
             raise CommandError(f"Failed to {command} one or more services")
