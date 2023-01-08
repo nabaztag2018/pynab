ec762930b75348ae73afddd087c7214665eb2c21
14roiron <yohann.roiron@mines-paristech.fr>
2023-01-08 18:11:58 +0100

    first test of MQTT integration

---
+++ nabmqttd/templatetags/mqtt_tags.py
@@ -0,0 +1,26 @@
+from django import template
+from django.template.defaultfilters import stringfilter
+
+register = template.Library()
+
+
+@register.filter
+@stringfilter
+def to_profile_url(value):
+    username = ""
+    instance = ""
+    parts = value.split("@")
+    if len(parts) == 2:
+        username = parts[0]
+        instance = parts[1]
+    elif len(parts) == 3 and parts[0] == "":
+        username = parts[1]
+        instance = parts[2]
+    else:
+        raise ValueError(
+            'Invalid mqtt identifier "'
+            + value
+            + '". Should be "name@instance.tld" or "@name@instance.tld"'
+        )
+
+    return "https://" + instance + "/@" + username
